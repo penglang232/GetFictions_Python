8a117ab1c4230c641477d6da0a0c6e84e21bc834
penglang232 <491322994@qq.com>
2019-06-14 15:59:26 +0800

    修改0614

--- getfiction.py
+++ getfiction.py
@@ -4,21 +4,6 @@ import chardet
 import sys
 from pyquery import PyQuery
 
-def readDocument(url_path):
-    context = ssl._create_unverified_context()
-    url = url_path
-    request = urllib.request.Request(url)
-    response = urllib.request.urlopen(url=request,context=context)
-    content = response.read()
-    global typeEncode
-    typeEncode = sys.getfilesystemencoding()##系统默认编码
-    global infoencode
-    infoencode = chardet.detect(content).get('encoding','utf-8')##通过第3方模块来自动提取网页的编码
-    #print (typeEncode + " " + infoencode)
-    html = content.decode(infoencode,'ignore').encode(typeEncode)##先转换成unicode编码，然后转换系统编码输出
-    #print (html)
-    return html
-
 # if len(sys.argv)<2 :
 #    print ('参数数量不正确！')
 #    sys.exit()
@@ -27,18 +12,23 @@ fileName = []
 with open('fileList.txt', 'r+') as f0:
     for line in f0:
         line=line.strip('\n')
+        if line.startswith('#'):
+            continue
         fileName.append(line)
 
 print (fileName)
 for path in fileName:
-    html = readDocument(path)
 
-    d = PyQuery(html)
+    d = PyQuery(path)
     title = d('title').html()
-    title = title.encode(infoencode).decode(typeEncode,'ignore')
+    #title = title.encode(infoencode).decode(typeEncode,'ignore')
     print (title)
 
     path = title + ".html"
     with open(path,'wb+') as f1:
-	    f1.write(html)
+	    f1.write(d.html().encode())
 
+    #author = d('div.msg em:first').html()
+    author = d('meta[name=description]').html()
+    #author = author.encode(infoencode).decode(typeEncode,'ignore')
+    print (author)
--- recordcode.py
+++ recordcode.py
@@ -14,3 +14,23 @@ url = 'https://www.88dush.com/'
 
 f = open(path,'w+',encoding='utf-8')
 f.write(xxx)
+
+
+
+def readDocument(url_path):
+    context = ssl._create_unverified_context()
+    url = url_path
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(url=request,context=context)
+    content = response.read()
+    global typeEncode
+    typeEncode = sys.getfilesystemencoding()##系统默认编码
+    global infoencode
+    infoencode = chardet.detect(content).get('encoding','utf-8')##通过第3方模块来自动提取网页的编码
+    infoencode = 'GBK' if infoencode=='GB2312' else infoencode
+    #print (typeEncode + " " + infoencode)
+    html = content.decode(infoencode,'ignore').encode(typeEncode)##先转换成unicode编码，然后转换系统编码输出
+    #print (html)
+    return html
+	
+#html = readDocument(path)
